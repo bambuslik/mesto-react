7df09d07e8440913b71e52e17a1377ebbb5384a7
Kostya Zhi <smart.index@gmail.com>
2020-10-18 01:00:01 +0300

    Updates

---
+++ precache-manifest.302fd6c1555c55951a109b9a19edc0b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee5783c9a175cb352681ca2c63747da",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "5218cca5437e812259eb",
+    "url": "/mesto-react/static/css/main.fa2683c1.chunk.css"
+  },
+  {
+    "revision": "a209c6bbce83e17987c9",
+    "url": "/mesto-react/static/js/2.464ddcfc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mesto-react/static/js/2.464ddcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5218cca5437e812259eb",
+    "url": "/mesto-react/static/js/main.b2441353.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/mesto-react/static/media/add-btn.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/mesto-react/static/media/edit-btn.58afbe73.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/edit-profile-hover.d476a8b8.svg"
+  },
+  {
+    "revision": "9b809a1b37577533dbfb302c85629aef",
+    "url": "/mesto-react/static/media/form-close-btn.9b809a1b.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/mesto-react/static/media/like-btn-active.9fdebb6b.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/mesto-react/static/media/like-btn.6c74d9b5.svg"
+  },
+  {
+    "revision": "94cbab21c102b09abc17acfd1e75c465",
+    "url": "/mesto-react/static/media/logo.94cbab21.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/mesto-react/static/media/trash.efa6fb04.svg"
+  }
+]);
